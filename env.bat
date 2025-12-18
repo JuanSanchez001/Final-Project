cb45da28f7118d539db2f97906de2f53d7a8760b
Juan Sanchez <sanchezjuan4159@gmail.com>
2025-12-18 13:18:21 -0800

    Feat: complete template and add play button

---
+++ env.bat
@@ -0,0 +1,7 @@
+set SECRET_KEY=kdlasof
+set GITHUB_CLIENT_ID=Ov23litXalBQwl5ZhRGi
+set GITHUB_CLIENT_SECRET=818e590131fb2599f82099e5e1a526f0acbc8a34
+set MONGO_CONNECTION_STRING=mongodb+srv://user0:c8Qr3VhifRPMuxzT@cluster0.qxop8tp.mongodb.net/?appName=Cluster0
+set MONGO_DBNAME=WinsDB
+
+ 
