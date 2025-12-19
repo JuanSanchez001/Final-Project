a4e4c9d8da24b61af99324d3611fcc575e776de9
Juan Sanchez <sanchezjuan4159@gmail.com>
2025-12-19 13:45:52 -0800

    Style: restyle format of menu screen

--- webapp.py
+++ webapp.py
@@ -93,7 +93,7 @@ def renderPage1():
         user_data_pprint = '';
     return render_template('page1.html',dump_user_data=user_data_pprint)
     
-@app.route('/play')
+@app.route('/play', methods=['GET', 'POST'])
 def play_button():
     return redirect(url_for('renderPage2'))
 
